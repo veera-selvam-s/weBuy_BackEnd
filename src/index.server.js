c8fd6566f69f87d25d82a461069064b74c3f4d9d
web-veera <veeraselvam965@gmail.com>
2021-09-02 11:56:13 +0530

    2-user-signup

---
+++ src/index.server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const env = require('dotenv');
+const app = express();
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+
+//routes
+const userRoutes = require ('./routes/user');
+
+
+//environment variable or const
+env.config();
+
+//mongodb connection cloud
+//`mongodb+srv://root:<password>@cluster0.mfxqr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+// mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.mfxqr.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, 
+// {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,  
+// }).then(() => {
+//     console.log('Connection estabislished with MongoDB');
+// })
+// .catch(error => console.error(error.message));
+
+//local connection
+mongoose.connect(`mongodb://127.0.0.1:27017/`, 
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+     
+}).then(() => {
+    console.log('Connection estabislished with MongoDB');
+})
+.catch(error => console.error(error.message));
+
+
+//middleware
+app.use(bodyParser());
+app.use('/api',userRoutes);
+
+
+
+//listen - start server
+app.listen(process.env.PORT,()=>{
+    console.log(`server is running on port ${process.env.PORT} `);
+});
