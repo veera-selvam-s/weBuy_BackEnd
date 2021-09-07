a36cdfc2786f0513bfa05c7515edf79cfafae4d5
web-veera <veeraselvam965@gmail.com>
2021-09-07 13:10:52 +0530

    6-create-get-category

--- src/controller/category.js
+++ src/controller/category.js
@@ -1,6 +1,27 @@
 const Category = require('../models/category');
 const slugify = require('slugify');
 
+function createCategories(categories, parentId = null) {
+    const categoryList = [];
+    let category;
+    if (parentId == null) {
+      category = categories.filter((cat) => cat.parentId == undefined);
+    } else {
+      category = categories.filter((cat) => cat.parentId == parentId);
+    }
+  
+    for (let cate of category) {
+      categoryList.push({
+        _id: cate._id,
+        name: cate.name,
+        slug: cate.slug,
+        children: createCategories(categories, cate._id),
+      });
+    }
+  
+    return categoryList;
+  }
+
 exports.addcategory = (req, res) => {
     const categoryObj = {
         name: req.body.name,
@@ -18,10 +39,13 @@ exports.addcategory = (req, res) => {
     })
 
 }
-exports.getCategories = (req,res)=>{
+exports.getCategories = (req, res) => {
     Category.find({})
-    .exec((error,categories)=>{
-        if (error) return res.status(400).json({ error });
-        if (categories) return res.status(201).json({ categories });
-    });
+        .exec((error, categories) => {
+            if (error) return res.status(400).json({ error });
+            if (categories) {
+                const categoryList = createCategories(categories);
+                res.status(200).json({ categoryList });
+              }
+        });
 }
