4bb6d734325dc7c65b97bd04d02003c564c777fc
web-veera <veeraselvam965@gmail.com>
2021-09-09 10:38:36 +0530

    7-19-41-product-upload

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { requireSignin, adminMiddleware } = require('../common-middleware/index');
+const { createProduct } = require('../controller/product');
 // const { addcategory, getCategories } = require("../controller/category")
-const router = express.Router();
-const Product = require('../models/product')
 
-router.post('/product/create', requireSignin, adminMiddleware, (req,res)=>{
-    res.status(200).json({message:"hello"})
-});
+const  multer = require ('multer');
+const upload = multer({dest:'uploads/'});
+const router = express.Router();
+
+
+router.post('/product/create', requireSignin, upload.single('productPicture'),adminMiddleware, createProduct);
 // router.get('/category/getCategory', getCategories);
 module.exports = router;
