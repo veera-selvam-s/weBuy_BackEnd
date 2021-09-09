bf9a5b4611ab7878248dda966692287bc479fc65
web-veera <veeraselvam965@gmail.com>
2021-09-09 12:39:59 +0530

    7-29-multiple-products

--- src/controller/product.js
+++ src/controller/product.js
@@ -2,5 +2,5 @@ const Product = require('../models/product');
 const shortid = require('shortid');
 
 exports.createProduct = (req, res) => {
-    res.status(200).json({ file: req.file, body: req.body });
+    res.status(200).json({ file: req.files, body: req.body });
 };
--- src/routes/product.js
+++ src/routes/product.js
@@ -3,11 +3,22 @@ const { requireSignin, adminMiddleware } = require('../common-middleware/index')
 const { createProduct } = require('../controller/product');
 // const { addcategory, getCategories } = require("../controller/category")
 
-const  multer = require ('multer');
-const upload = multer({dest:'uploads/'});
+const multer = require('multer');
 const router = express.Router();
+const shortid = require('shortid');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(path.dirname(__dirname), "uploads"));
+    },
+    filename: function (req, file, cb) {
+      cb(null, shortid.generate() + "-" + file.originalname);
+    },
+  });
+const upload = multer({ storage });
 
 
-router.post('/product/create', requireSignin, upload.single('productPicture'),adminMiddleware, createProduct);
+router.post('/product/create', requireSignin, adminMiddleware, upload.array('productPicture'),createProduct);
 // router.get('/category/getCategory', getCategories);
 module.exports = router;
