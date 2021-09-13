bb6a9f0fb2c23e00376150d89fca551940f9c0f3
web-veera <veeraselvam965@gmail.com>
2021-09-13 15:42:22 +0530

    7-36-16-product

--- src/controller/product.js
+++ src/controller/product.js
@@ -1,6 +1,33 @@
 const Product = require('../models/product');
 const shortid = require('shortid');
+const slugify = require('slugify')
 
 exports.createProduct = (req, res) => {
-    res.status(200).json({ file: req.files, body: req.body });
+    //res.status(200).json({ file: req.files, body: req.body });
+
+    const {
+      name,price,description,category,createdBy
+    } = req.body;
+    let productPictures =[];
+
+    if (req.files.length > 0){
+      productPictures=req.files.map(file=>{
+        return {img:file.filename}
+      })
+    }
+
+    const product = new Product({
+      name: req.body.name,
+      slug: slugify(name),
+      price,
+      description,
+      productPictures,
+      category,
+      createdBy:req.user._id
+    });
+    product.save(((error,product)=>{
+      if(error) return res.status(400).json({error});
+      if(product) return res.status(201).json({product})
+    }));
+
 };
