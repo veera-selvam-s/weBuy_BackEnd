cb055d0409037b6f6819a5a63f445c65aea045a3
web-veera <veeraselvam965@gmail.com>
2021-12-06 15:35:08 +0000

    13_cmplete

--- src/index.server.js
+++ src/index.server.js
@@ -3,6 +3,7 @@ const env = require('dotenv');
 const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 //routes
 const authRoutes = require('./routes/auth');
@@ -38,6 +39,7 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO
 
 
 //middleware
+app.use(cors());
 app.use(express.json());
 app.use('/public',express.static(path.join(__dirname,'uploads')));
 app.use('/api', authRoutes);
