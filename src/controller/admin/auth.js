fa8eca2a0609ddaf2c559a07ca118e3bba0648e8
web-veera <veeraselvam965@gmail.com>
2021-12-13 13:12:33 +0530

    17_8 hash password

--- src/controller/admin/auth.js
+++ src/controller/admin/auth.js
@@ -1,97 +1,98 @@
-const User = require('../../models/user');
-const jwt = require('jsonwebtoken');
+const User = require("../../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const shortid = require("shortid");
 
 exports.signup = (req, res) => {
-	console.log("[+]Request to signup");
-    User.findOne({
-        email: req.body.email
-    })
-        .exec((error, user) => {
+  console.log("[+]Request to signup");
+  User.findOne({
+    email: req.body.email
+  }).exec(async (error, user) => {
+    if (user)
+      return res.status(400).json({
+        message: "admin already registered"
+      });
+    //destucture req body
+    const { firstName, lastName, email, password } = req.body;
+    const hash_password = await bcrypt.hash(password, 10);
 
-            if (user) return res.status(400).json({
-                message: 'admin already registered'
-            });
-            //destucture req body
-            const {
-                firstName,
-                lastName,
-                email,
-                password
-            } = req.body;
-            const _user = new User({
-                firstName,
-                lastName,
-                email,
-                password,
-                username: Math.random().toString(),
-                role: 'admin'
-            });
+    const _user = new User({
+      firstName,
+      lastName,
+      email,
+      hash_password,
+      password,
+      username: Math.random().toString(),
+      role: "admin"
+    });
 
-            _user.save((error, data) => {
-                if (error) {
-					console.log("[+]Error from saving signup");
-                    return res.status(400).json({
-                        message: 'something went wrong'
-                    });
-                }
-                if (data) {
-					console.log("[+]Success from signup");
-                    return res.status(201).json({
-                        message: 'Admin created successfully..!'
-                    });
-                }
-            });
-
-            if (error) {
-                return res.status(400).json({
-                    message: "error accured"
-                });
-            }
-
+    _user.save((error, data) => {
+      if (error) {
+        console.log("[+]Error from saving signup");
+        return res.status(400).json({
+          message: "something went wrong"
         });
-}
+      }
+      if (data) {
+        console.log("[+]Success from signup");
+        return res.status(201).json({
+          message: "Admin created successfully..!"
+        });
+      }
+    });
+
+    if (error) {
+      return res.status(400).json({
+        message: "error accured"
+      });
+    }
+  });
+};
 
 exports.signin = (req, res) => {
-	console.log("[+]Request to signin");
-    User.findOne({ email: req.body.email })
-        .exec((error, user) => {
-            if (error) return res.status(400).json({ error });
-            if (user) {
-                if (user.authenticate(req.body.password) && user.role === 'admin') {
-					
-                    const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '30d' });
-                    const { _id, firstName, lastName, email, role, fullName } = user;
-                    res.cookie('token',token,{expiresIn:'30d'});
-                    res.status(200).json({
-                        token,
-                        user: {
-                            _id, firstName, lastName, email, role, fullName
-                        }
-                    });
-                    console.log("[+]Success from Signin");
+  console.log("[+]Request to signin");
+  User.findOne({ email: req.body.email }).exec((error, user) => {
+    if (error) return res.status(400).json({ error });
+    if (user) {
+      if (user.authenticate(req.body.password) && user.role === "admin") {
+        const token = jwt.sign(
+          { _id: user._id, role: user.role },
+          process.env.JWT_SECRET,
+          { expiresIn: "30d" }
+        );
+        const { _id, firstName, lastName, email, role, fullName } = user;
+        res.cookie("token", token, { expiresIn: "30d" });
+        res.status(200).json({
+          token,
+          user: {
+            _id,
+            firstName,
+            lastName,
+            email,
+            role,
+            fullName
+          }
+        });
+        console.log("[+]Success from Signin");
+      } else {
+        console.log("[+]Error from Signin");
+        return res.status(400).json({
+          message: "invalid password"
+        });
+      }
+    } else {
+      console.log("[+]Error from Signin");
+      return res.status(400).json({ message: "something went wrong" });
+    }
+  });
+};
 
-                } else {
-					console.log("[+]Error from Signin");
-                    return res.status(400).json({
-                        message: 'invalid password'
-                    });
-                }
-
-
-            } else {
-				console.log("[+]Error from Signin");
-                return res.status(400).json({ message: 'something went wrong' });
-            }
-        })
-}
-
-//signout 
+//signout
 exports.signout = (req, res) => {
-	console.log("[+]Request to Signout");
-    res.clearCookie('token');
-    res.status(200).json({
-		
-        message: 'sign out successfully'
-    });
-    console.log("[+]Success from Signout");
-}
+  console.log("[+]Request to Signout");
+  res.clearCookie("token");
+  res.status(200).json({
+    message: "sign out successfully"
+  });
+  console.log("[+]Success from Signout");
+};
--- src/controller/auth.js
+++ src/controller/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const shortid = require("shortid");
 
 
 exports.signup = (req, res) => {
@@ -9,7 +11,7 @@ exports.signup = (req, res) => {
     User.findOne({
         email: req.body.email
     })
-        .exec((error, user) => {
+        .exec(async(error, user) => {
             if (user) return res.status(400).json({
                 message: 'User already registered'
             });
@@ -20,11 +22,12 @@ exports.signup = (req, res) => {
                 email,
                 password
             } = req.body;
+            const hash_password = await bcrypt.hash(password, 10);
             const _user = new User({
                 firstName,
                 lastName,
                 email,
-                password,
+                hash_password,
                 username: Math.random().toString()
             });
 
