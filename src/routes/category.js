bad67eac936f29a6f496771daa8b4bf81ea008f8
web-veera <veeraselvam965@gmail.com>
2021-12-18 12:06:16 +0530

    21_updateCategory

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { requireSignin, adminMiddleware } = require('../common-middleware/index');
-const { addcategory, getCategories } = require("../controller/category")
+const { addcategory, getCategories, updateCategories,deleteCategories } = require("../controller/category")
 const router = express.Router();
 const shortid = require('shortid');
 const path = require('path');
@@ -18,4 +18,6 @@ const storage = multer.diskStorage({
 
 router.post('/category/create', requireSignin, adminMiddleware, upload.single('categoryImage'), addcategory);
 router.get('/category/getCategory', getCategories);
+router.post('/category/update', upload.array('categoryImage'), updateCategories);
+router.post('/category/delete', deleteCategories);
 module.exports = router;
