39a79f6af90d44674d847af5c2199e64a9797932
web-veera <veeraselvam965@gmail.com>
2021-12-24 12:17:48 +0530

    35_cart

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { requireSignin, userMiddleware } = require('../common-middleware/index');
-const { addItemToCart } = require("../controller/cart")
+const { addItemToCart, addToCart, getCartItems } = require('../controller/cart');
 const router = express.Router();
 
 router.post('/user/cart/addtocart', requireSignin, userMiddleware, addItemToCart);
+//router.post('/user/cart/addToCartByLogin', requireSignin, userMiddleware, addToCart);
+router.post('/user/getCartItems', requireSignin, userMiddleware, getCartItems);
+
 
 module.exports = router;
