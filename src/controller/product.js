77d909703fe0a5762318d7a78f4c78989fdd5d12
web-veera <veeraselvam965@gmail.com>
2021-12-26 09:52:02 +0530

    42_undefinedPage

--- src/controller/product.js
+++ src/controller/product.js
@@ -1,20 +1,17 @@
-const Product = require('../models/product');
-const shortid = require('shortid');
-const slugify = require('slugify');
-const Category = require('../models/category');
+const Product = require("../models/product");
+const shortid = require("shortid");
+const slugify = require("slugify");
+const Category = require("../models/category");
 
 exports.createProduct = (req, res) => {
-
   //res.status(200).json( { file: req.files, body: req.body } );
 
-  const {
-    name, price, description, category, quantity, createdBy
-  } = req.body;
+  const { name, price, description, category, quantity, createdBy } = req.body;
   let productPictures = [];
 
   if (req.files.length > 0) {
-    productPictures = req.files.map(file => {
-      return { img: file.filename }
+    productPictures = req.files.map((file) => {
+      return { img: file.filename };
     });
   }
 
@@ -26,69 +23,71 @@ exports.createProduct = (req, res) => {
     description,
     productPictures,
     category,
-    createdBy: req.user._id
+    createdBy: req.user._id,
   });
 
-  product.save(((error, product) => {
+  product.save((error, product) => {
     if (error) return res.status(400).json({ error });
     if (product) {
       res.status(201).json({ product });
     }
-  }));
-
-
+  });
 };
 
 exports.getProductsBySlug = (req, res) => {
   const { slug } = req.params;
   Category.findOne({ slug: slug })
-  .select('_id')
-  .exec((error, category) => {
-      if(error){
-          return res.status(400).json({error});
+    .select("_id type")
+    .exec((error, category) => {
+      if (error) {
+        return res.status(400).json({ error });
       }
 
-      if(category){
-          Product.find({ category: category._id })
-          .exec((error, products) => {
+      if (category) {
+        Product.find({ category: category._id }).exec((error, products) => {
+          if (error) {
+            return res.status(400).json({ error });
+          }
 
-              if(error){
-                  return res.status(400).json({error});
-              }
-
-              if(products.length > 0){
-                  res.status(200).json({
-                      products,
-                      productsByPrice: {
-                          under5k: products.filter(product => product.price <= 5000),
-                          under10k: products.filter(product => product.price > 5000 && product.price <= 10000),
-                          under15k: products.filter(product => product.price > 10000 && product.price <= 15000),
-                          under20k: products.filter(product => product.price > 15000 && product.price <= 20000),
-                          under30k: products.filter(product => product.price > 20000 && product.price <= 30000),
-                      }
-                  });
-              }
-
-              
-          })
+          if (category.type) {
+            if (products.length > 0) {
+              res.status(200).json({
+                products,
+                productsByPrice: {
+                  under5k: products.filter((product) => product.price <= 5000),
+                  under10k: products.filter(
+                    (product) => product.price > 5000 && product.price <= 10000
+                  ),
+                  under15k: products.filter(
+                    (product) => product.price > 10000 && product.price <= 15000
+                  ),
+                  under20k: products.filter(
+                    (product) => product.price > 15000 && product.price <= 20000
+                  ),
+                  under30k: products.filter(
+                    (product) => product.price > 20000 && product.price <= 30000
+                  ),
+                },
+              });
+            }
+          } else {
+            res.status(200).json({ products });
+          }
+        });
       }
-      
-
-      
-  });
-}
+    });
+};
 
 exports.getProductDetailsById = (req, res) => {
   const { productId } = req.params;
-  if(productId){
-      Product.findOne({ _id: productId })
-      .exec((error, product) => {
-          if(error) return res.status(400).json({ error });
-          if(product){
-              res.status(200).json({ product });
-          }
-      });
-  }else{
-      return res.status(400).json({ error: 'Params required' });
+  if (productId) {
+    Product.findOne({ _id: productId }).exec((error, product) => {
+      if (error) return res.status(400).json({ error });
+      if (product) {
+        res.status(200).json({ product });
+      }
+    });
+  } else {
+    return res.status(400).json({ error: "Params required" });
   }
-}
+};
